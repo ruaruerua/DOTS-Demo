0983b51cbb16555d19ad3f08930dc1786abc659a
ruaruerua <1065904251@qq.com>
2021-11-30 11:33:35 +0800

    官方教程，带修改

---
+++ ECSSamples/Assets/HelloCube/4. SpawnFromMonoBehaviour/Normal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class Normal : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int CountX = 100;
+    public int CountY = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (var x = 0; x < CountX; x++)
+        {
+            for (var y = 0; y < CountY; y++)
+            {
+                // Efficiently instantiate a bunch of entities from the already converted entity prefab
+                var gameObj = UnityEngine.Transform.Instantiate(Prefab);
+
+                // Place the instantiated entity in a grid with some noise
+                gameObj.transform.position = transform.TransformPoint(new float3(x * 1.3F, noise.cnoise(new float2(x, y) * 0.21F) * 2, y * 1.3F));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ECSSamples/Assets/HelloCube/4. SpawnFromMonoBehaviour/NormalRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class NormalRotate : MonoBehaviour
+{
+    public int rotateSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        transform.rotation = math.mul(math.normalize(transform.rotation),quaternion.AxisAngle(math.up(), rotateSpeed * deltaTime));
+    }
+}
